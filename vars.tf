eef8999cba2cc14751801078d5539ef6308ef791
excoRt911 <36086652+excoRt911@users.noreply.github.com>
2021-12-08 22:45:57 +0200

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -16,7 +16,7 @@ variable "ecs_auto_scale_role_name" {
 }
 
 variable "app_image" {
-  description = "Docker image to run in the ECS cluster"
+  description = "Docker image in AWS ECR to run in the ECS cluster"
   default     = "753392824297.dkr.ecr.us-east-1.amazonaws.com/daniel.b:latest"
 }
 
@@ -31,11 +31,11 @@ variable "app_count" {
 }
 
 variable "fargate_cpu" {
-  description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
+  description = "Fargate instance CPU units"
   default     = "256"
 }
 
 variable "fargate_memory" {
-  description = "Fargate instance memory to provision (in MiB)"
+  description = "Fargate instance memory"
   default     = "512"
 }
